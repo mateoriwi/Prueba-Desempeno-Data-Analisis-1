e021dfd698014109c2b89f5347b2148c03c52c1b
mateoriwi <sharonmilena.mmm@gmail.com>
2026-08-31 10:26:37 -0500

    Segundo commit

--- data/ficha_dataset.md
+++ data/ficha_dataset.md
@@ -4,7 +4,7 @@ Dataset
 
 ### Archivos descargados: (Retail 2009-10.csv) y (Retail 2010-11.csv)
 
-### Descripción : Es un dataframe con 525,461 filas y 8 columnas.
+### Descripción: Son 2 dataframes el primero con 525,461 filas y 8 columnas y el segundo con 541,910 filas y 8 columnas
 
 ### Trata sobre ventas en una tienda, las columnas son "Invoice" Que sería el id de la facturación, el "Stock Code" que sería el ID del producto a comprar, "Description" que describe o nos dice cómo es el producto, "Quantity" que es la cantidad que se compró, "InvoiceDate" Que es la fecha de la facturación, "price" que equivale al PRECIO UNITARIO de cada producto, "Customer ID" que sería el comprador, y "Country" que sería el país del comprador
 
--- etl.py
+++ etl.py
@@ -4,10 +4,102 @@ import psycopg2
 import sqlalchemy
 from sqlalchemy import create_engine
 
-df = pd.read_csv("data/Retail 2009-10.csv")
+df2009 = pd.read_csv("data/Retail 2009-10.csv")
+df2010 = pd.read_csv("data/Retail 2010-11.csv")
 
-print(df)
+df = pd.concat([df2009, df2010], ignore_index=True)
 
-df2 = pd.read_csv("data/Retail 2010-11.csv")
+clean_df = df.copy()
+raw_df = df.copy()
 
-print(df2)
+print("Nulos en el dataset")
+
+print("Duplicados en el dataset")
+print(clean_df.duplicated().sum())
+
+print(df.shape)
+
+clean_df["Description"] = clean_df["Description"].fillna("Unknown")
+
+print(clean_df.dtypes)
+
+clean_df["InvoiceDate"] = pd.to_datetime(
+    clean_df["InvoiceDate"],
+    format="mixed",
+    errors="coerce"
+)
+
+clean_df['Customer ID'] = pd.to_numeric(clean_df['Customer ID'], errors='coerce').astype('Int64')
+
+print(clean_df.dtypes)
+print(clean_df.isnull().sum())
+print(clean_df["InvoiceDate"])
+
+print(clean_df[clean_df["Description"].isnull()])
+print(clean_df[clean_df["Customer ID"] == "<NA>"])
+
+## Se decide eliminar TODAS las filas con "Description" = NaN y "Customer ID" = <NA> ya que estas, tampoco tienen precio ni valor monetario
+
+
+print(clean_df[(clean_df["Price"] > 0.0) & (clean_df["Description"].isna())])
+
+clean_df = clean_df.dropna(subset=["Description"])
+
+print(clean_df[(clean_df["Customer ID"].isna()) & (clean_df["Price"] == 0.0)])
+
+clean_df = clean_df.dropna(subset=["Customer ID"])
+
+clean_df = clean_df.drop_duplicates()
+
+print(clean_df[clean_df["Price"] < 0])
+
+print(clean_df[clean_df["Quantity"] < 0])
+
+clean_df["Revenue"] = (clean_df["Quantity"]) * (clean_df["Price"])
+
+print(clean_df)
+
+clean_df['Year'] = clean_df['InvoiceDate'].dt.year
+
+customer = clean_df[["Customer ID", "Country"]].drop_duplicates()
+
+products = clean_df[["StockCode", "Description"]].drop_duplicates()
+
+invoices = clean_df[["Invoice", "InvoiceDate", "Customer ID"]].drop_duplicates()
+
+transactions = clean_df[["Invoice", "StockCode", "Quantity", "Price", "Revenue"]] 
+
+transactions.insert(
+    0,
+    "TransactionID",
+    range(1, len(transactions) + 1))
+
+print(customer["Customer ID"].duplicated().sum())
+
+
+print(
+    products.groupby("StockCode")["Description"]
+    .nunique()
+    .sort_values(ascending=False)
+    .head(20)
+)
+
+
+print(
+    invoices.groupby("Invoice")["Customer ID"]
+    .nunique()
+    .sort_values(ascending=False)
+    .head()
+)
+
+
+print(
+    invoices.groupby("Invoice")["InvoiceDate"]
+    .nunique()
+    .sort_values(ascending=False)
+    .head()
+)
+
+
+print(( transactions["Quantity"] * transactions["Price"] == transactions["Revenue"] ).all())
+
